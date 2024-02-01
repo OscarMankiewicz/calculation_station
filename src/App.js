9d40ce2c3f8cb3c4de7c553cc97fe20e4ce5b0b4
OscarMankiewicz <omankiewicz77@gmail.com>
2024-02-01 14:39:53 -0500

    layout designed for website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './App.css';
+
 function App() {
+  return (
+    <div className="layout">
+      <header>Calculation Station</header>
+      <nav>Nav</nav>
+      <main>Main</main>
+      <article className='results'>Results</article>
+      <footer>Developed by Oscar Mankiewicz</footer>
+    </div>
+  );
 }
 
 export default App;
