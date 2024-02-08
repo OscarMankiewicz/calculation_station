53a13cd9be8dcfbc562fcbd8d41f958eb1970269
OscarMankiewicz <omankiewicz77@gmail.com>
2024-02-08 11:57:16 -0500

    created logic to handle rendering and switching calculators

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+// Import calculator components
+import renderCalculator from './components/CalculatorRenderer';
+
+//Import calculator data
+import calculatorData from './data/calculatorData';
+
+
 function App() {
+  const [selectedCalculator, setSelectedCalculator] = useState(calculatorData[0].id);
+
+  const handleCalculatorChange = (calculatorId) => {
+    setSelectedCalculator(calculatorId);
+  };
+
   return (
     <div className="layout">
       <header>Calculation Station</header>
-      <nav>Nav</nav>
-      <main>Main</main>
+      <nav>
+        {calculatorData.map(calculator => (
+          <button key={calculator.id} onClick={() => handleCalculatorChange(calculator.id)}>
+            {calculator.label}
+          </button>
+        ))}
+      </nav>
+      <main>
+        {renderCalculator(selectedCalculator)}
+      </main>
       <article className='results'>Results</article>
       <footer>Developed by Oscar Mankiewicz</footer>
     </div>
