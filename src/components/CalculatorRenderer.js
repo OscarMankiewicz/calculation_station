be0bc33ba93ce2dfe4550a9a314bbcd5a6ac418f
OscarMankiewicz <omankiewicz77@gmail.com>
2024-02-14 13:53:35 -0500

    updated to handle new calcs

--- src/components/CalculatorRenderer.js
+++ src/components/CalculatorRenderer.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import TipCalculator from './TipCalculator';
+import DistanceConverter from './DistanceConverter';
+import VolumeConverter from './VolumeConverter';
 
 const renderCalculator = (selectedCalculator) => {
     switch (selectedCalculator) {
-      case 'tip':
-        return <TipCalculator />;
-      default:
-        return null;
+        case 'tip':
+            return <TipCalculator />;
+        case 'distance':
+            return <DistanceConverter />;
+        case 'volume':
+            return <VolumeConverter />;
+        default:
+            return null;
     }
 };
 
--- src/data/calculatorData.js
+++ src/data/calculatorData.js
@@ -1,5 +1,7 @@
 const calculatorData = [
     { id: 'tip', label: 'Tip Calculator' },
+    { id: 'distance', label: 'Distance Converter' },
+    { id: 'volume', label: 'Volume Converter' },
 ];
 
 export default calculatorData;
