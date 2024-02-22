ee19635819be721df4a8519590170ca37fb5eda9
OscarMankiewicz <omankiewicz77@gmail.com>
2024-02-21 22:35:17 -0500

    added data and handles for renderer

--- src/components/CalculatorRenderer.js
+++ src/components/CalculatorRenderer.js
@@ -2,6 +2,10 @@ import React from 'react';
 import TipCalculator from './TipCalculator';
 import DistanceConverter from './DistanceConverter';
 import VolumeConverter from './VolumeConverter';
+import MassConverter from './MassConverter';
+import HypotenuseCalculator from './HypotenuseCalculator';
+import SpeedConverter from './SpeedConverter';
+import DigitalStorageConverter from './DigitalStorageConverter';
 
 const renderCalculator = (selectedCalculator) => {
     switch (selectedCalculator) {
@@ -11,6 +15,14 @@ const renderCalculator = (selectedCalculator) => {
             return <DistanceConverter />;
         case 'volume':
             return <VolumeConverter />;
+        case 'mass':
+            return <MassConverter />;
+        case 'speed':
+            return <SpeedConverter />;
+        case 'digital-storage':
+            return <DigitalStorageConverter />;
+        case 'hypotenuse':
+            return <HypotenuseCalculator />;
         default:
             return null;
     }
--- src/data/calculatorData.js
+++ src/data/calculatorData.js
@@ -2,6 +2,10 @@ const calculatorData = [
     { id: 'tip', label: 'Tip Calculator' },
     { id: 'distance', label: 'Distance Converter' },
     { id: 'volume', label: 'Volume Converter' },
+    { id: 'mass', label: 'Mass Converter'},
+    { id: 'speed', label: 'Speed Converter' },
+    { id: 'digital-storage', label: 'Digital Storage Converter' },
+    { id: 'hypotenuse', label: 'Hypotenuse Calculator' },
 ];
 
 export default calculatorData;
